83bfc05ee43e215d15a676444fffb67888786ce5
Najam Ali Abbas <90936436+Ctoic@users.noreply.github.com>
2023-02-21 23:14:02 +0500

    Created screen , fruit and snake.

---
+++ Snake.py
@@ -0,0 +1,93 @@
+# snake game using turtle graphics
+import turtle
+import time
+import random
+
+delay = 0.1
+
+# Set up the screen
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("grey")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+# Create the snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0, 0)
+head.direction = "stop"
+
+# Create the food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0, 100)
+
+# Define the functions for moving the snake
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+
+
+# Set up keyboard bindings
+wn.listen()
+wn.onkeypress(go_up, "Up")
+wn.onkeypress(go_down, "Down")
+wn.onkeypress(go_right, "Right")
+wn.onkeypress(go_left, "Left")
+
+# Set up the main game loop
+while True:
+    wn.update()
+
+    # Check for collision with the wall
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "stop"
+
+    # Check for collision with the food
+    if head.distance(food) < 20:
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        food.goto(x, y)
+
+    # Move the snake
+    move()
+
+    time.sleep(delay)
+
+wn.mainloop()
